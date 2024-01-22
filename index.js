af324d22fe5425a5a1ad04845d5515b78e91e21c
Christopher Brock <brock.christopherj@gmail.com>
2024-01-22 10:53:35 -0600

    Exercise 3.11 Add frontend

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body' ))
 let numbers = [
