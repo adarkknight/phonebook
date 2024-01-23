266a3a585b1f49fe51aa9bd775b16b22e17346b3
Christopher Brock <brock.christopherj@gmail.com>
2024-01-23 13:27:28 -0600

    Exercise 3.17 add put request handler

--- index.js
+++ index.js
@@ -90,12 +90,26 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 app.get("/info", (request, response) => {
   response.send(
     `<p>Phonebook has info for ${numbers.length} people</p>
          <p>${new Date()}</p>`
   );
 });
+
 app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
