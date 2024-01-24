457ea2dca32fd50c10a2a7346cb9099bf1ece94d
Christopher Brock <brock.christopherj@gmail.com>
2024-01-24 11:26:51 -0600

    Exercise 3.19

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -26,28 +28,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let numbers = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 app.get("/api/persons", (request, response) => {
   Person.find({}, { __v: 0 }).then((people) => {
     response.json(people);
@@ -66,22 +46,25 @@ app.get("/api/persons/:id", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({
+  //     error: "content missing",
+  //   });
+  // }
 
   const number = new Person({
     name: body.name,
     number: body.number,
   });
-  number.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  number
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
@@ -93,12 +76,13 @@ app.delete("/api/persons/:id", (request, response, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  const { name, number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: query }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
--- models/person.js
+++ models/person.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 const url = process.env.MONGODB_URI;
 
-console.log('connecting to url', url);
+console.log("connecting to url", url);
 
-mongoose.connect(url)
-.then(result => {
-    console.log('connected to MongoDB')
-})
-.catch((error) => {
-    console.log('error connecting to MongoDB', error.message);
-})
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB", error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-})
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: String,
+});
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model("Person", personSchema);
